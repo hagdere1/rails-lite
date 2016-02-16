0b81821513455d88cf99afd04c1af3a2cecc9810
Haig Gregory Agdere <hagdere1@binghamton.edu>
2016-02-15 22:17:05 -0500

    Finish Router. Debug ControllerBase.

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,9 +8,11 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
     @req = req
     @res = res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
@@ -20,22 +22,32 @@ class ControllerBase
 
   # Set the response status code and header
   def redirect_to(url)
-    raise Exception if already_built_response?
-    res['Location'] = url
-    res.status = 302
+    raise "double render error" if already_built_response?
+
+    @res['Location'] = url
+    @res.status = 302
+
     @already_built_response = true
-    session.store_session(res)
+
+    session.store_session(@res)
+
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    raise Exception if already_built_response?
-    res['Content-Type'] = content_type
-    res.body = [content]
+    raise "double render error" if already_built_response?
+
+    @res['Content-Type'] = content_type
+    @res.write(content)
+
     @already_built_response = true
-    session.store_session(res)
+
+    session.store_session(@res)
+
+    nil
   end
 
   # use ERB and binding to evaluate templates
@@ -48,10 +60,14 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
-    @session ||= Session.new(req)
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
-  def invoke_action(name)
+  def invoke_action(action_name)
+    self.send(action_name)
+    render(action_name) unless already_built_response?
+
+    nil
   end
 end
--- lib/router.rb
+++ lib/router.rb
@@ -16,7 +16,13 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+    names = match_data.names
+    captures = match_data.captures
 
+    route_params = Hash[names.zip(captures)]
+
+    @controller_class.new(req, res, route_params).invoke_action(action_name)
   end
 end
 
@@ -24,29 +30,40 @@ class Router
   attr_reader :routes
 
   def initialize
-    @routes = ""
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
-    @route = Route.new(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    matching_route = match(req)
+    if matching_route.nil?
+      res.status = 404
+    else
+      matching_route.run(req, res)
+    end
   end
 end
